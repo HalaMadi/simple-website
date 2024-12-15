a8d2f02973f360a693a298ccd65f9ab9106022f9
HalaMadi <Halamadi75@gmail.com>
2024-12-15 23:15:25 +0200

    Style Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import './style.css'
+const Navbar = () => {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg py-4">
+                <div className="container container-fluid">
+                    <a className="navbar-brand text-white fs-3 fw-bold" href="#">START BOOTSTRAP</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon" />
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0 d-flex gap-4">
+                        <li className="nav-item">
+                                <a className="nav-link active text-white fw-bold" aria-current="page" href="#">PORTFOLIO</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active text-white fw-bold" aria-current="page" href="#">ABOUT</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active text-white fw-bold" aria-current="page" href="#">CONTACT</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
